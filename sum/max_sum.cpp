d0eb08d0bf6caa09935f9e4395e3c41d75d15d10
WS-only <2450320887@qq.com>
2016-10-12 22:53:02 +0800

    20161012

---
+++ sum/max_sum.cpp
@@ -0,0 +1,22 @@
+/*
+问题描述：给出一个数列，计算顺序中最大的和，输出结果包括最大的和，从第几个元素开始，第几个元素结束
+          用户可以设置测试组数。
+时间：2016/10/12
+作者：WS
+备注：未完成。数组个数定义不会 
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cout<<"Please input the number of cases(1~20):";
+	cin>>n;
+	for(int i=1 ;i<=n ;i++)
+	{
+		
+	}
+	return 0;
+} 
